223cc7b1edbfaaf20e769b2f41e6b339f5585f8a
Mariana Caradec <maricaradec@gmail.com>
2024-09-03 16:57:05 -0300

    agregado de dotenv

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
 import './App.css'
+
 import { NavBar } from './components/NavBar/NavBar'
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer'
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer'
